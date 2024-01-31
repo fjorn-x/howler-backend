3f0ef5541e7538233dfe8a5611d8973840fc6458
fjorn-x <hamzashaikh.cs@gmail.com>
2024-01-31 20:21:51 +0530

    verification ssl 10

--- src/main/java/com/hamza/howler/controllers/TestController.java
+++ src/main/java/com/hamza/howler/controllers/TestController.java
@@ -1,16 +1,11 @@
 package com.hamza.howler.controllers;
 
 import com.hamza.howler.response.AuthResponse;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 @RestController
 public class TestController {
@@ -21,14 +16,17 @@ public class TestController {
     }
 
     @GetMapping("/.well-known/pki-validation/6B7EF7003F13D16B83A0718BB760E5A9.txt")
-    public ResponseEntity<Resource> ssl()throws Exception{
+    public ResponseEntity<String> ssl()throws Exception{
 
-        Path path = Path.of("C:\\JavaProjects\\howler\\howler-backend\\src\\main\\resources\\6B7EF7003F13D16B83A0718BB760E5A9.txt");
-        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+//        Path path = Path.of("C:\\JavaProjects\\howler\\howler-backend\\src\\main\\resources\\6B7EF7003F13D16B83A0718BB760E5A9.txt");
+//        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
 
+        String res="3A2954B035CEA6DDE3F12F4C3F2006EE437146AFBC4B9EF4AF3F857E21899414\n" +
+                "comodoca.com\n" +
+                "bce187cd9561df6";
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .body(resource);
+                .body(res);
 
     }
 }
